5924dcb736c4234e15ff56388466c88a64ecac5d
Ary Gimenez <argi.prog@gmail.com>
2025-08-23 04:39:37 -0300

    ---

--- EpicGYM/vite.config.ts
+++ EpicGYM/vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    // Base de la URL para el despliegue de GitHub Pages
+  base: '/Web-APA/',
   plugins: [react()],
 })
