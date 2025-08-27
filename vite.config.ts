74e0593dc8cfacf4be393a10c47e259323ee6d91
Ary <65731115+AryGimenez@users.noreply.github.com>
2025-08-27 12:34:45 -0300

    ----

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     // Base de la URL para el despliegue de GitHub Pages
-  base: '/Web-APA/',
+  base: '/EpicGYM/',
   plugins: [react()],
 })
